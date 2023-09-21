81e8df3265a1a07abcdc0208cf4bfdce9721921f
divinejoshua <divineuche399@gmail.com>
2023-09-22 00:44:31 +0100

    Completed nginx config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>This is the frontend</h1>
+        <h3>The backend is on the <a className="App-link" href="/api">/api</a> link</h3>
       </header>
     </div>
   );
